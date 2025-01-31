2338a0f7c41e39d7487d081ce0db069c4043f70b
danilofumuso <danilofumuso@gmail.com>
2025-01-31 16:17:59 +0100

    wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {
+  HTTP_INTERCEPTORS,
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
+import { TokenInterceptor } from './auth/token.interceptor';
+import { FooterComponent } from './main-components/footer/footer.component';
+import { NavbarComponent } from './main-components/navbar/navbar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [AppComponent, FooterComponent, NavbarComponent],
+  imports: [BrowserModule, AppRoutingModule, NgbModule],
+  providers: [
+    provideHttpClient(withInterceptorsFromDi()),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true,
+    },
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NgbModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
